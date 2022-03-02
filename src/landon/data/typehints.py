c3df0d7caf80e01dbb43b7a0183f734e71843bc7
felix <felix@detry.com.ar>
2022-03-02 19:54:19 +0100

    s

---
+++ src/landon/data/typehints.py
@@ -0,0 +1,23 @@
+from typing import TypedDict
+
+
+class CreateFlightSchema(TypedDict):
+    flightnumber: str
+    aircraft: str
+    departure_airport: str
+    arrival_airport: str
+    game_url: str
+    date: str
+    time: str
+    roavhub_ping: bool
+
+
+class Movie(TypedDict):
+    flightnumber: str
+    aircraft: str
+    departure_airport: str
+    arrival_airport: str
+    game_url: str
+    date: str
+    time: str
+    roavhub_ping: bool
